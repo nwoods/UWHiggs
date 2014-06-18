1b4227bd92705d37c658e83b8d7587ce8e2d73e1
Nate Woods <nwoods@hep.wisc.edu>
2014-06-18 11:34:44 -0500

    Un-comment lines to delete stuff before running

--- zz/run_dataonly.sh
+++ zz/run_dataonly.sh
@@ -9,15 +9,20 @@ source jobid.sh
 #export jobid=$jobid7
 #rake analyzebprime
 
-#if [ -e results/2014-03-03-8TeV/ZZAnalyzerEEEE/data_Run2012A.json ]
-#then
-#	rm results/*/*/*.json
-#fi
+if [ -e results/2014-03-03-8TeV/ZZAnalyzerEEEE/data_Run2012A.json ]
+then
+	rm results/*/*/*.json
+fi
 
-#if [ -e HZZ_passed.txt ]
-#then
-#	rm HZZ_passed.txt
-#fi
+if [ -e HZZ_passed.txt ]
+then
+	rm HZZ_passed.txt
+fi
+
+if [ -e results/2014-03-03-8TeV/ZZAnalyzerMMMM/data_DoubleMuParked_Run2012B_22Jan2013_v1.root ]
+then
+    rm results/*/*/*data*
+fi
 
 export jobid=$jobid8
 rake analyzeZZ_dataonly
