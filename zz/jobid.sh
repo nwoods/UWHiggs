987a1c46ef34578aa9e2c3ca09bd6d42e1d3dde1
Nate Woods <nwoods@hep.wisc.edu>
2014-06-25 04:30:40 -0500

    Change to new ntuples

--- zz/jobid.sh
+++ zz/jobid.sh
@@ -1,3 +1,3 @@
 export jobid7='yyyy-mm-dd-7TeV'
-export jobid8='2014-03-03-8TeV'
+export jobid8='ZZ_ntuples_bestasof_24-6-2014'
 
--- zz/run.sh
+++ zz/run.sh
@@ -9,20 +9,20 @@ source jobid.sh
 #export jobid=$jobid7
 #rake analyzebprime
 
-#if [ -e results/2014-03-03-8TeV/ZZAnalyzerEEEE/data_Run2012A.json ]
-#then
-#	rm results/*/*/*.json
-#fi
+if [ -e results/2014-03-03-8TeV/ZZAnalyzerEEEE/data_Run2012A.json ]
+then
+	rm results/*/*/*.json
+fi
 
-#if [ -e HZZ_passed.txt ]
-#then
-#	rm HZZ_passed.txt
-#fi
-#
-#if [ -e HZZ_all_official.txt ]
-#then
-#	rm HZZ_all_official.txt
-#fi
+if [ -e HZZ_passed.txt ]
+then
+	rm HZZ_passed.txt
+fi
+
+if [ -e HZZ_all_official.txt ]
+then
+	rm HZZ_all_official.txt
+fi
 
 export jobid=$jobid8
 rake analyzeZZ
