803f57e251c7813914ac670693a31cf0fa1b018b
Nate Woods <nwoods@hep.wisc.edu>
2014-08-29 03:15:39 -0500

    Fix install script to work with 13 TeV and not crash for pre-installed FSA

--- install.sh
+++ install.sh
@@ -10,6 +10,10 @@ ${CMSSW_BASE:?"Please run cmsenv before running install.sh"}
 set -o errexit
 set -o nounset
 
+# Check CMSSW version
+export MAJOR_VERSION=`echo $CMSSW_VERSION | sed "s|CMSSW_\([0-9]\)_.*|\1|"`
+export MINOR_VERSION=`echo $CMSSW_VERSION | sed "s|CMSSW_\([0-9]\)_\([0-9]\)_.*|\2|"`
+
 echo "Symlinking FinalStateAnalysis into working area"
 
 pushd $CMSSW_BASE/src
@@ -18,6 +22,27 @@ if ! [ -L FinalStateAnalysis ]; then
     ln -s UWHiggs/dependencies/FinalStateAnalysis FinalStateAnalysis
   fi
 fi
+
+echo "Selectring FSA version..."
+pushd FinalStateAnalysis
+if [ `git remote -v | grep upstream | grep uwcms | wc -l` = 2 ];
+then
+    echo 'Upstream detected'
+else
+    echo 'Adding upstream'
+    git remote add upstream git@github.com:uwcms/FinalStateAnalysis.git
+fi
+git fetch upstream
+if [ "$MAJOR_VERSION" -eq "7" ];
+then
+    echo "Selecting recipe for 13TeV miniAOD"
+    git checkout -b miniAOD upstream/miniAOD_dev
+elif [ "$MAJOR_VERSION" -eq "5" ];
+then
+    echo "Selecting recipe for 8TeV"
+    git checkout -b uwcms_master upstream/master
+fi
+popd
 
 echo "Checking out FSA dependencies"
 pushd FinalStateAnalysis/recipe
